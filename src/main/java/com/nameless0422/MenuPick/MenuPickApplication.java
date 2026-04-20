00d1dd28efd0a60348adaae406ab7122f5440ae4
nameless0422 <cjy042218@gmail.com>
2026-04-21 08:27:56 +0900

    [init] init

---
+++ src/main/java/com/nameless0422/MenuPick/MenuPickApplication.java
@@ -0,0 +1,13 @@
+package com.nameless0422.MenuPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nameless0422/MenuPick/MenuPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nameless0422.MenuPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
